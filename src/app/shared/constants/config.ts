7922bf8f668d855459c7e1eeddc71d07ca6781b4
janchester27 <chester@sitesphil.com>
2026-02-03 10:10:27 +0800

    updated API url

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    // "API_URL": "http://192.168.1.74:5000/"
-    "API_URL": "http://localhost:5000/"
+    "API_URL": "http://192.168.1.45:5000/"
+    // "API_URL": "http://localhost:5000/"
     // "API_URL": "http://localhost:5000/"
 }
